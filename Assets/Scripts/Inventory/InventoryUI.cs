3f63a62996cf9cd013a04f748cf8ee76076e3eb8
Swiiner <almenar.killian@hotmail.com>
2024-12-14 17:55:11 +0100

    Debut station

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
--- Assets/Scripts/Planets/CelestialBody.cs
+++ Assets/Scripts/Planets/CelestialBody.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
